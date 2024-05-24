0bbb5b452696ed163fc0d0294c5975f59312acfb
Olivier <Olivier@DESKTOP-Q76BK1H>
2024-05-24 16:29:22 +0200

    Ajoutez des fichiers projet.

---
+++ pizza_mama/Models/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_mama.Models
+{
+    // Notre model de pizza
+    public class Pizza
+    {
+        public int PizzaID { get; set; }
+
+        [Display(Name ="Nom")]
+        public string Nom {  get; set; }
+
+        [Display(Name ="Prix")]
+        public float Prix { get; set; }
+
+        [Display(Name ="Végétarienne")]
+        public bool Vegetarienne { get; set; }
+
+        [Display(Name = "Ingrédients")]
+        public string Ingredients { get; set; }
+
+    }
+}
